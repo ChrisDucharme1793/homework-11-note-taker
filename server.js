d1ed5e270c01e7c1e8f8cd36c489f3640dcd7247
Chris Ducharme <ducharme.christophert@gmail.com>
2020-04-08 18:06:17 -0500

    heroku push

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencues
+//npm packages or modules that application will use
+var express = require("express");
+
+//Tells node that we are creating an express server
+var app = express();
+
+// Sets a port to listen to
+var PORT = process.env.PORT || 3000;
+
+//Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+//Routes a map for our server on how to responed when users visit or request data for urls(seems to always be API and HTML routes)
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//Starts server to listen
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
